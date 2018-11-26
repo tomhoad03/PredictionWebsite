73db39b2ae7a93cbe21e1f94ca678180555f363c
Tom-Hoad <thomas.hoad@btopenworld.com>
2018-11-26 13:33:33 +0000

    I got a proper POST request working on the login webpage.

--- src/server/controllers/UserController.java
+++ src/server/controllers/UserController.java
@@ -1,15 +1,22 @@
 package server.controllers;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import server.models.User;
+
+        import javax.ws.rs.*;
+        import javax.ws.rs.core.MediaType;
 
 @Path("user/")
 public class UserController {
+
+    //Defines the HTTP request as post.
     @POST
-    @Path("get")
+    @Path("login")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.TEXT_PLAIN)
+
+    //Inputs the parameters of the form and then uses then outputs them again.
     public String loginHandler(@FormParam("loginUsername") String loginUsername,
                                @FormParam("loginPassword") String loginPassword) {
-        return loginUsername + loginPassword;
+        return "Login details are: " + loginUsername + " " + loginPassword;
     }
 }
