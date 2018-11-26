8b1198b5818470ffba37f9d977646d6c797bcd8b
Tom-Hoad <thomas.hoad@btopenworld.com>
2018-11-26 12:22:29 +0000

    UserController

--- src/server/controllers/UserController.java
+++ src/server/controllers/UserController.java
@@ -1,18 +1,15 @@
 package server.controllers;
 
-import javax.ws.rs.FormParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("html/")
+@Path("user/")
 public class UserController {
     @POST
-    @Path("login")
-    @Produces(MediaType.TEXT_HTML)
+    @Path("get")
+    @Produces(MediaType.TEXT_PLAIN)
     public String loginHandler(@FormParam("loginUsername") String loginUsername,
                                @FormParam("loginPassword") String loginPassword) {
-        return "You have logged in!";
+        return loginUsername + loginPassword;
     }
 }
